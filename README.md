45320f49608668e8a1871911a9ed72c4c98e2911
hugomatic <hugo@hugomatic.ca>
2017-03-28 13:01:51 -0700

    added vimrc and setup from repo

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # unix configs #
 
+setup.bash
 
+will prompt you to copy .vimrc
+
+for a root .vimrc, use "sudo su" before running, as sudo only will not work.
---
+++ setup.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "DIR: $DIR"
+
+vimrc="$HOME/.vimrc"
+
+confirm "replace $vimrc?" && cp $DIR/vimrc $vimrc
+
+
+# read -r -p "copy \"$DIR/vimrc\" to \"$vimrc\"? [y/N] " response
+# response=${response,,}    # tolower
+# if [[ "$response" =~ ^(yes|y)$ ]]
+# then
+#   cp $DIR/vimrc $vimrc
+#   stat $vimrc
+# else
+#   echo "skipping vimrc"
+# fi
