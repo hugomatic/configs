2dd5d996d6f41359df859cbddef4fe771dca8c7e
hugomatic <hugo@hugomatic.ca>
2017-04-20 07:51:51 -0700

    added udelv script

---
+++ udelv/cd.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# mounts disk if the directory is not present. needs sudo
+
+code="/media/hugo/Space/code"
+if stat $code;
+then
+  echo "disk already mounted"
+else
+  sudo mount /dev/sdb3 /media/hugo/Space
+  cd ~
+  ln -s /media/hugo/Space/code code
+  ln -s /media/hugo/Space/dev dev
+  ln -s /media/hugo/Space/install install
+fi
+
+cd $code
