a447c53de66ac579cf9f9192fff1b4e2c5d23e59
hugomatic <hugo@hugomatic.ca>
2017-04-24 09:12:46 -0700

    transcode has output file name parameter

--- transcode.bash
+++ transcode.bash
@@ -3,4 +3,30 @@
 # recordmydesktop generates out.ogv
 # this script creates out.mp4
 
-/usr/bin/ffmpeg -y -i out.ogv -s 1280x720 -aspect 16:9 -r 30000/1001 -b 2M -bt 4M -pass 1 -vcodec libx264 -threads 0 -an -f mp4 -loglevel verbose ./out.mp4
+output="out"
+if [ -z "$1" ]
+  then
+    echo "no output name supplied"
+  else
+    output=$1
+fi
+
+echo "output: $output.mp4 and $output.ogv"
+
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+
+/usr/bin/ffmpeg -y -i out.ogv -s 1280x720 -aspect 16:9 -r 30000/1001 -b 2M -bt 4M -pass 1 -vcodec libx264 -threads 0 -an -f mp4 -loglevel verbose "./$output.mp4"
+cp out.ogv "$output.ogv"
+
